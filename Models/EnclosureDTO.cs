22a832c13c9a5186037ee81aacaeae9d0fda37f2
ebram101 <144430149+ebram101@users.noreply.github.com>
2024-07-01 18:40:48 +0200

    created api

--- Models/EnclosureDTO.cs
+++ Models/EnclosureDTO.cs
@@ -1,27 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Zoo.Enums;
+﻿using Zoo.Enums;
 
 namespace Zoo.Models
 {
-    public class Enclosure
+    public class EnclosureDTO
     {
         public int Id { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
-        [Required]
         public Climate Climate { get; set; }
-
-        [Required]
         public HabitatType HabitatType { get; set; }
-
-        [Required]
         public SecurityLevel SecurityLevel { get; set; }
-
-        [Required]
         public double Size { get; set; }
-
-        public ICollection<Animals> Animals { get; set; }
+        public List<ForeignAnimalDTO> Animals { get; set; }
     }
 }
